d2f8bf824d4039d1948faa09be6320d4cf6ffbf8
annisa <92286817+fdhannisa@users.noreply.github.com>
2024-07-16 16:06:38 +0700

    update halaman dashbord

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -7,31 +7,38 @@ document.addEventListener('DOMContentLoaded', function() {
       labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
       datasets: [
         {
-          label: 'Kategori 1',
+          label: 'Kantor UP BL',
           data: [12, 19, 3, 5, 2, 3, 7, 8, 6, 10, 12, 15],
           backgroundColor: 'rgba(22, 79, 99, 0.2)',
           borderColor: 'rgba(22, 79, 99, 1)',
           borderWidth: 1
         },
         {
-          label: 'Kategori 2',
+          label: 'PLTA Way Besai',
           data: [7, 11, 5, 8, 3, 7, 10, 6, 9, 12, 14, 9],
           backgroundColor: 'rgba(60, 186, 159, 0.2)',
           borderColor: 'rgba(60, 186, 159, 1)',
           borderWidth: 1
         },
         {
-          label: 'Kategori 3',
+          label: 'PLTA Batu Tegi',
           data: [5, 6, 9, 10, 4, 8, 11, 3, 5, 7, 11, 13],
           backgroundColor: 'rgba(255, 206, 86, 0.2)',
           borderColor: 'rgba(255, 206, 86, 1)',
           borderWidth: 1
         },
         {
-          label: 'Kategori 4',
+          label: 'PLTG Tarahan',
           data: [10, 13, 4, 7, 9, 5, 8, 12, 11, 6, 8, 10],
           backgroundColor: 'rgba(75, 192, 192, 0.2)',
           borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        },
+         {
+          label: 'PLTG Teluk Betung',
+          data: [10, 13, 4, 7, 9, 5, 8, 12, 11, 6, 8, 10],
+          backgroundColor: 'rgba(153, 102, 255, 0.2)',
+          borderColor: 'rgba(153, 102, 255, 1)',
           borderWidth: 1
         }
       ]
