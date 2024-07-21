8c031b79887f0b6350da78e5b12715031d9381a7
annisa <92286817+fdhannisa@users.noreply.github.com>
2024-07-21 12:35:49 +0700

    menambah halaman kelola jadwal pemeriksaan, kelola data alat, petugas, dan laporan komdisi alat

--- assets/js/chart3.js
+++ assets/js/chart3.js
@@ -7,19 +7,25 @@ document.addEventListener('DOMContentLoaded', function() {
       labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
       datasets: [{
         label: 'Tahun 2024',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [98.0, 92.3, 97.8, 100.0, 90.6, 98,8],
         backgroundColor: 'rgba(22, 79, 99, 0.2)', // Warna dengan transparansi
         borderColor: 'rgba(22, 79, 99, 1)', // Warna tanpa transparansi
         borderWidth: 1
       }]
     },
     options: {
-      responsive: true,
-      scales: {
-        y: {
-          beginAtZero: true
+          responsive: true,
+          scales: {
+            y: {
+              beginAtZero: false,
+              min: 80,
+              ticks: {
+                callback: function(value) {
+                  return value + '%';
+                }
+              }
+            }
+          }
         }
-      }
-    }
+      });
   });
-}); 
